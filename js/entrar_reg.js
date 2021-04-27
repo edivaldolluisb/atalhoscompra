ba24686e32cecf137cfcf0c1267e550dbda57292
edivaldolb <50288937+edivaldolluisb@users.noreply.github.com>
2021-04-27 04:18:55 +0100

    overlay
    adicionei o efeito overlay e ajustes

---
+++ js/entrar_reg.js
@@ -0,0 +1,15 @@
+function openEntrar() {
+  document.getElementById("Entrar").style.height = "100%";
+}
+
+function closeEntrar() {
+  document.getElementById("Entrar").style.height = "0%";
+}
+
+function openReg() {
+  document.getElementById("Registrar_Tela").style.width = "100%";
+}
+
+function closeReg() {
+  document.getElementById("Registrar_Tela").style.width = "0%";
+}
