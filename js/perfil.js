c9f04c974919210b23cb308daf8ccff92590444e
edivaldolb <50288937+edivaldolluisb@users.noreply.github.com>
2021-05-07 11:35:10 +0100

    pagina de perfil

--- js/perfil.js
+++ js/perfil.js
@@ -6,6 +6,7 @@ let email = document.getElementById('email')
 let morada = document.getElementById('morada')
 let palavrapasse = document.getElementById('palavrapasse')
 var concluir_edit = document.getElementById('concluir_edit')
+var cancelar_edit = document.getElementById('cancelar_edit')
 
 
 btn_editar_perfil.addEventListener('click', () => {
@@ -17,5 +18,21 @@ btn_editar_perfil.addEventListener('click', () => {
     morada.removeAttribute('readonly')
     palavrapasse.removeAttribute('readonly')
     concluir_edit.style.display='block'
+    cancelar_edit.style.display='block'
+    btn_editar_perfil.style.display='none'
+})
+cancelar_edit.addEventListener('click', () => {
+
+    concluir_edit.style.display='none'
+    cancelar_edit.style.display='none'
+    btn_editar_perfil.style.display='block' 
+    //ao clicar no botão de manter, os dados vão voltar a ser apenas de leitura   
+    nome.removeAttribute('readonly', true)
+    ultimo_nome.setAttribute('readonly', true)
+    telefone.setAttribute('readonly', true)
+    email.setAttribute('readonly', true)
+    morada.setAttribute('readonly', true)
+    palavrapasse.setAttribute('readonly', true)
+    window.alert("os dados não foram alterados")
 })
 
