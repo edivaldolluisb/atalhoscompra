a1e26cfae189c5e24b9840025545b892450b5ed6
edivaldolb <50288937+edivaldolluisb@users.noreply.github.com>
2021-05-12 18:27:24 +0100

    input de quantidade
    troca do input de quantidade do carinho de compras

--- js/carrinho.js
+++ js/carrinho.js
@@ -6,3 +6,15 @@ function carinho_off() {
     window.document.getElementById('carinho_compras').style.display="none"
     
 }
+let btn_Diminuir = document.querySelector('.diminuir')
+let btn_Aumentar = document.querySelector('.aumentar')
+let input_QT = document.querySelector('input')
+
+btn_Aumentar.addEventListener('click', () =>{
+    input_QT.value = parseInt(input_QT.value) + 1
+
+})
+btn_Diminuir.addEventListener('click', () =>{
+    input_QT.value = parseInt(input_QT.value) - 1
+
+})
