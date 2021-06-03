f569dfb519c9d57f2e1892311b4a1aa4ecaf7110
edivaldolb <50288937+edivaldolluisb@users.noreply.github.com>
2021-06-03 17:36:41 +0100

    bug_text
    texo da página inicial que não aparecia corrigido

--- js/slide_de_textos.js
+++ js/slide_de_textos.js
@@ -1,27 +1,16 @@
+/*slide de imagens*/
 
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 0;
+showSlides();
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showSlides() {
   var i;
-  var slides = window.document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+  setTimeout(showSlides, 2000); // Mudar a imagem  cada 2 segundos
+} 
